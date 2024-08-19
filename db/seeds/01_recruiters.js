d6cda124418fb776fab7d0c0e04dd5ca6c53b85c
Kassym Turysbek <91659281+kassymturysbek@users.noreply.github.com>
2024-08-20 02:01:04 +0500

    seedsAd

--- db/seeds/01_recruiters.js
+++ db/seeds/01_recruiters.js
@@ -1,12 +1,8 @@
 exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
     return knex('recruiters').del()
         .then(function() {
-            // Inserts seed entries
             return knex('recruiters').insert([
-                { name: "Galvanize Recruiter", email: 'admin@admin.com', password: "password" },
-                { name: "Jess Jones", email: 'jess@jones.com', password: "pass" },
-                { name: "James Smith", email: 'james@smith.com', password: "pass" }
+                { name: "Emil Hasanov", email: 'emil.r.hasanov@gmail.com', password: "password", id: 2 },
             ]);
         });
 };
--- db/seeds/02_tests.js
+++ db/seeds/02_tests.js
@@ -1,14 +1,8 @@
 exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
     return knex('tests').del()
         .then(function() {
-            // Inserts seed entries
             return knex('tests').insert([
-                { name: "JavaScript", total: 10, code: "sdsa2342", recruiters_id: 1 },
-                { name: "Node.js", total: 10, code: "123sdasada", recruiters_id: 1 },
-                { name: "Java", total: 10, code: "sds6049", recruiters_id: 1 },
-                { name: "C++", total: 10, code: "sds4029", recruiters_id: 1 },
-                { name: "Python", total: 10, code: "sds3881", recruiters_id: 1 },
+                { name: "Belbin Test", total: 7, code: "BelbinTestCode", recruiters_id: 2, belbinSwitch: "on", id: 1 },
             ]);
         });
 };
